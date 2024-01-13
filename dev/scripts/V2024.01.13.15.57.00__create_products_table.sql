f326bdfd0eddc60478bd039a23609376ee5466da
Sathish Prakasam <sathish.k.prakasam@gmail.com>
2024-01-13 18:28:30 +0530

    Auto increment sequence

---
+++ dev/scripts/V2024.01.13.15.57.00__create_products_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Products (
+                         Id int,
+                         Brand varchar(50),
+                         ProductName varchar(50),
+                         ProductType smallint,
+                         ProductDetailsId smallint,
+                         City varchar(255),
+                         UpdateDate datetime,
+                         CreateDate datetime
+);
+
+ALTER TABLE Products AUTO_INCREMENT=1001;
